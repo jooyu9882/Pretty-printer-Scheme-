40aa9a81185f82b8eecf8b3e637e1373411084d1
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 13:33:21 -0600

    Update Scanner.java

--- prog1/Parse/Scanner.java
+++ prog1/Parse/Scanner.java
@@ -13,142 +13,133 @@ import Tokens.IntToken;
 import Tokens.StrToken;
 
 public class Scanner {
-	private PushbackInputStream in;
+    private PushbackInputStream in;
 
-	// Maximum length of strings and identifers
-	private int BUFSIZE = 1000;
-	private byte[] buf = new byte[BUFSIZE];
+    // Maximum length of strings and identifiers
+    private int BUFSIZE = 1000;
+    private byte[] buf = new byte[BUFSIZE];
 
-	public Scanner(InputStream i) {
-		in = new PushbackInputStream(i);
-	}
+    public Scanner(InputStream i) {
+        in = new PushbackInputStream(i);
+    }
 
-	public Token getNextToken() {
-		int ch;
+    public Token getNextToken() {
+        int ch;
 
-		try {
-			// It would be more efficient if we'd maintain our own
-			// input buffer and read characters out of that
-			// buffer, but reading individual characters from the
-			// input stream is easier.
-			ch = in.read();
+        try {
+            ch = in.read();
 
-			// TODO: Skip white space and comments
-			while (ch == ' ' || ch == '\t' || ch == '\n' || ch == ';') {
+            // Skip whitespace and comments
+            while (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' || ch == '\f' || ch == ';') {
+                if (ch == ';') {
+                    while (ch != '\n' && ch != -1)
+                        ch = in.read();
+                } else {
+                    ch = in.read();
+                }
+            }
 
-				if (ch == ';') {
-					while (ch != '\n' && ch != -1)
-						ch = in.read();
-				}
+            // EOF
+            if (ch == -1)
+                return null;
 
-				else
-					ch = in.read();
-			}
-			// Return null on EOF
-			if (ch == -1)
-				return null;
+            // Special characters
+            else if (ch == '\'')
+                return new Token(TokenType.QUOTE);
+            else if (ch == '(')
+                return new Token(TokenType.LPAREN);
+            else if (ch == ')')
+                return new Token(TokenType.RPAREN);
+            else if (ch == '.')
+                return new Token(TokenType.DOT);
 
-			// Special characters
-			else if (ch == '\'')
-				return new Token(TokenType.QUOTE);
-			else if (ch == '(')
-				return new Token(TokenType.LPAREN);
-			else if (ch == ')')
-				return new Token(TokenType.RPAREN);
-			else if (ch == '.')
-				// We ignore the special identifier `...'.
-				return new Token(TokenType.DOT);
+            // Boolean constants
+            else if (ch == '#') {
+                ch = in.read();
+                if (ch == 't' || ch == 'T')
+                    return new Token(TokenType.TRUE);
+                else if (ch == 'f' || ch == 'F')
+                    return new Token(TokenType.FALSE);
+                else if (ch == -1) {
+                    System.err.println("Unexpected EOF following #");
+                    return null;
+                } else {
+                    System.err.println("Illegal character '" + (char) ch + "' following #");
+                    return getNextToken();
+                }
+            }
 
-			// Boolean constants
-			else if (ch == '#') {
-				ch = in.read();
+            // String constants
+            else if (ch == '"') {
+                int i = 0;
+                while (true) {
+                    ch = in.read();
+                    if (ch == '"') {
+                        return new StrToken(new String(buf, 0, i));
+                    } else if (ch == -1) {
+                        System.err.println("Unexpected EOF in string");
+                        return null;
+                    } else {
+                        buf[i++] = (byte) ch;
+                        if (i >= BUFSIZE) {
+                            System.err.println("String too long");
+                            return null;
+                        }
+                    }
+                }
+            }
 
-				if (ch == 't')
-					return new Token(TokenType.TRUE);
-				else if (ch == 'f')
-					return new Token(TokenType.FALSE);
-				else if (ch == -1) {
-					System.err.println("Unexpected EOF following #");
-					return null;
-				} else {
-					System.err.println("Illegal character '" +
-							(char)ch + "' following #");
-					return getNextToken();
-				}
-			}
+            // Integer constants
+            else if (ch >= '0' && ch <= '9') {
+                int value = ch - '0';
+                while (true) {
+                    ch = in.read();
+                    if (ch >= '0' && ch <= '9') {
+                        value = value * 10 + (ch - '0');
+                    } else {
+                        if (ch != -1)
+                            in.unread(ch);
+                        break;
+                    }
+                }
+                return new IntToken(value);
+            }
 
-			// String constants
-			else if (ch == '"') {
-				// TODO: Scan a string into the buffer variable buf
-				int i = 0;
-				while (true) {
-					ch = in.read();
-					if (ch == '"') {
-						return new StrToken(new String(buf,0, i));
-					} 
+            // Identifiers
+            else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') ||
+                     "!$%&*/:<=>?~_^+-".indexOf(ch) != -1) {
 
-					else if (ch == -1) {
-						System.err.println("Unexpected EOF in string");
-						return null;
-					}
+                int i = 0;
+                buf[i++] = (byte) ch;
 
-					else {
-						buf[i++] = (byte)ch;
-					}
-				}
-			}
+                while (true) {
+                    ch = in.read();
+                    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') ||
+                        (ch >= '0' && ch <= '9') ||
+                        "!$%&*/:<=>?~_^+-".indexOf(ch) != -1) {
+                        buf[i++] = (byte) ch;
+                        if (i >= BUFSIZE) {
+                            System.err.println("Identifier too long");
+                            return null;
+                        }
+                    } else {
+                        if (ch != -1) in.unread(ch);
+                        break;
+                    }
+                }
 
-			// Integer constants
-			else if (ch >= '0' && ch <= '9') {
-				int i = ch - '0';
-				// TODO: scan the number and convert it to an integer
-				 while (true) {
-					ch = in.read();
+                return new IdentToken(new String(buf, 0, i).toLowerCase()); // lowercase for Scheme
+            }
 
-					if (ch >= '0' && ch <= '9') {
-						i = i * 10 + (ch - '0');
-					}
-					
-					else {
-						in.unread(ch);
-						break;
-					}
-				}
+            // Illegal character
+            else {
+                System.err.println("Illegal input character '" + (char) ch + "'");
+                return getNextToken();
+            }
 
-				// Put the character after the integer back into the input
-				// in.unread(ch);
-				return new IntToken(i);
-			}
-
-			// Identifiers
-			else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
-			    int i = 0;
-			    // Read first character
-			    buf[i++] = (byte) ch;
-			    
-			    // Read subsequent characters (letters or digits)
-			    while (true) {
-			        ch = in.read();
-			        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
-			            buf[i++] = (byte) ch;
-			        } else {
-			            // put back the non-identifier character
-			            if (ch != -1) in.unread(ch);
-			            break;
-			        }
-			    }
-			    
-			    return new IdentToken(new String(buf, 0, i));
-			}
-
-			// Illegal character
-			else {
-				System.err.println("Illegal input character '" + (char)ch + '\'');
-				return getNextToken();
-			}
-		} catch (IOException e) {
-			System.err.println("IOException: " + e.getMessage());
-			return null;
-		}
-	}
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+            return null;
+        }
+    }
 }
