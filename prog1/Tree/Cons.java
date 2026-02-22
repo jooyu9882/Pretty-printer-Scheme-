0fcafae4ce031d06dd0214e4ebd823a0ba20b54f
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 13:04:00 -0600

    Update Cons.java

--- prog1/Tree/Cons.java
+++ prog1/Tree/Cons.java
@@ -21,48 +21,22 @@ public class Cons extends Node {
     public Cons(Node a, Node d) {
         car = a;
         cdr = d;
-        parseList();
+        form = new Regular(); // default
     }
 
-    // parseList() `parses' special forms, constructs an appropriate
-    // object of a subclass of Special, and stores a pointer to that
-    // object in variable form. It would be possible to fully parse
-    // special forms at this point. Since this causes complications
-    // when using (incorrect) programs as data, it is easiest to let
-    // parseList only look at the car for selecting the appropriate
-    // object from the Special hierarchy and to leave the rest of
-    // parsing up to the interpreter.
-    void parseList() {
-    
-
-    // TODO: Add any helper functions for parseList
-
-    form = new Regular();
-
-    if (car instanceof Ident) {
-
-        String name = ((Ident) car).getName();
-
-        if (name.equals("if"))
-            form = new If();
-        else if (name.equals("lambda"))
-            form = new Lambda();
-        else if (name.equals("quote"))
-            form = new Quote();
-        else if (name.equals("begin"))
-            form = new Begin();
-        else if (name.equals("define"))
-            form = new Define();
-        else if (name.equals("set!"))
-            form = new Set();
-        else if (name.equals("let"))
-            form = new Let();
-        else if (name.equals("cond"))
-            form = new Cond();
-    }
+    // Allow Parser to set the Special form
+    public void setSpecial(Special s) {
+        form = s;
     }
 
-    // to the class hierarchy as needed.
+    // Accessors for car and cdr
+    public Node getCar() {
+        return car;
+    }
+
+    public Node getCdr() {
+        return cdr;
+    }
 
     public void print(int n) {
         form.print(this, n, false);
