8004c3bd48c6ff076265db185ee535f0ae2c1194
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 13:26:30 -0600

    Update Set.java

--- prog1/Special/Set.java
+++ prog1/Special/Set.java
@@ -3,9 +3,33 @@
 package Special;
 
 import Tree.Node;
+import Tree.Cons;
+import Tree.Nil;
 
 public class Set extends Special {
- 
+
+    @Override
     public void print(Node t, int n, boolean p) {
+        if (!(t instanceof Cons)) return;
+        Cons cons = (Cons) t;
+
+        // print opening line
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.print("(set! ");
+
+        // first element after "set!" (the variable)
+        Node varNode = cons.getCdrCar();
+        varNode.print(0, false);
+
+        // rest of expression(s), e.g., value(s)
+        Node valueNodes = cons.getCdrCdr();
+        while (valueNodes instanceof Cons) {
+            Node val = ((Cons) valueNodes).getCar();
+            System.out.print(" "); // space between elements
+            val.print(0, false);
+            valueNodes = ((Cons) valueNodes).getCdr();
+        }
+
+        System.out.println(")");
     }
 }
