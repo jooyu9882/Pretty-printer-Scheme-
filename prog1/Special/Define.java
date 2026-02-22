659c4de199f3bd466512b3203b1e7c0709fea26e
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 13:25:14 -0600

    Update Define.java

--- prog1/Special/Define.java
+++ prog1/Special/Define.java
@@ -1,11 +1,30 @@
-// Define -- Parse tree node strategy for printing the special form define
-
 package Special;
 
 import Tree.Node;
+import Tree.Cons;
 
 public class Define extends Special {
- 
+
+    @Override
     public void print(Node t, int n, boolean p) {
+        if (!(t instanceof Cons)) return;
+        Cons cons = (Cons) t;
+
+        Node first = cons.getCdrCar();
+        Node rest = cons.getCdrCdr();
+
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.print("(define ");
+        first.print(0, false);
+        System.out.println();
+
+        Node current = rest;
+        while (current instanceof Cons) {
+            ((Cons) current).getCar().print(n + 2, false);
+            current = ((Cons) current).getCdr();
+        }
+
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.println(")");
     }
 }
