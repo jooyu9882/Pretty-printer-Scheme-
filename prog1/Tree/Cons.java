9affee4e88a5d4a9dcbc232a5539d9f8e8fdeed8
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 15:03:05 -0600

    Update Cons.java

--- prog1/Tree/Cons.java
+++ prog1/Tree/Cons.java
@@ -1,9 +1,9 @@
-// Cons -- Parse tree node class for representing a Cons nodepackage Tree;
 package Tree;
 
 import Special.*;
 
 public class Cons extends Node {
+
     private Node car;
     private Node cdr;
     private Special form;
@@ -11,42 +11,53 @@ public class Cons extends Node {
     public Cons(Node a, Node d) {
         car = a;
         cdr = d;
-        form = new Regular(); // default
+        form = new Regular();   // default printing behavior
     }
 
-    // Allow Parser to set the Special form
+    // Parser assigns a special form printer (define, if, lambda...)
     public void setForm(Special s) {
         form = s;
     }
 
-    // Accessors for car and cdr
+    // Accessors
     public Node getCar() { return car; }
     public Node getCdr() { return cdr; }
 
-    // Convenience methods used by Special subclasses
+    // Convenience navigation helpers used by Special classes
     public Node getCdrCar() {
-        if (cdr instanceof Cons) return ((Cons)cdr).getCar();
+        if (cdr instanceof Cons)
+            return ((Cons)cdr).getCar();
         return null;
     }
 
     public Node getCdrCdr() {
-        if (cdr instanceof Cons) return ((Cons)cdr).getCdr();
+        if (cdr instanceof Cons)
+            return ((Cons)cdr).getCdr();
         return null;
     }
 
     public Node getCdrCdrCar() {
-        Node cdr2 = getCdrCdr();
-        if (cdr2 instanceof Cons) return ((Cons)cdr2).getCar();
+        Node temp = getCdrCdr();
+        if (temp instanceof Cons)
+            return ((Cons)temp).getCar();
         return null;
     }
 
     public Node getCdrCdrCdrCar() {
-        Node cdr3 = getCdrCdr();
-        if (cdr3 instanceof Cons) return ((Cons)cdr3).getCdrCar();
+        Node temp = getCdrCdr();
+        if (temp instanceof Cons)
+            return ((Cons)temp).getCdrCar();
         return null;
     }
 
-    // Printing methods
-    public void print(int n) { form.print(this, n, false); }
-    public void print(int n, boolean p) { form.print(this, n, p); }
+    // Printing dispatch (THIS is the key)
+    @Override
+    public void print(int n) {
+        form.print(this, n, false);
+    }
+
+    @Override
+    public void print(int n, boolean p) {
+        form.print(this, n, p);
+    }
 }
