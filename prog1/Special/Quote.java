842f09601b5cc393c6c20b682268abae7538846c
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 13:19:07 -0600

    Update Quote.java

--- prog1/Special/Quote.java
+++ prog1/Special/Quote.java
@@ -1,11 +1,27 @@
-// Quote -- Parse tree node strategy for printing the special form quote
-
 package Special;
 
 import Tree.Node;
+import Tree.Cons;
+import Tree.Nil;
 
 public class Quote extends Special {
- 
+
+    @Override
     public void print(Node t, int n, boolean p) {
+        if (!(t instanceof Cons)) return;
+        Cons cons = (Cons) t;
+        Node quoted = cons.getCdrCar(); // assume this gives the second element
+
+        // print quote character
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.print("'");
+
+        // if the quoted element is a list, print as a regular list
+        if (quoted instanceof Cons) {
+            new Regular().print(quoted, 0, false);
+        } else {
+            quoted.print(0, false);
+        }
+        System.out.println();
     }
 }
