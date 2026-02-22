73a44b55b1e9c101b3057451f1869cbc6c54bb7c
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 11:43:35 -0600

    Update Parser.java

--- prog1/Parse/Parser.java
+++ prog1/Parse/Parser.java
@@ -1,58 +1,95 @@
-// Parser -- the parser for the Scheme printer and interpreter
-//
-// Defines
-//
-//   class Parser;
-//
-// Parses the language
-//
-//   exp  ->  ( rest
-//         |  #f
-//         |  #t
-//         |  ' exp
-//         |  integer_constant
-//         |  string_constant
-//         |  identifier
-//    rest -> )
-//         |  exp+ [. exp] )
-//
-// and builds a parse tree.  Lists of the form (rest) are further
-// `parsed' into regular lists and special forms in the constructor
-// for the parse tree node class Cons.  See Cons.parseList() for
-// more information.
-//
-// The parser is implemented as an LL(0) recursive descent parser.
-// I.e., parseExp() expects that the first token of an exp has not
-// been read yet.  If parseRest() reads the first token of an exp
-// before calling parseExp(), that token must be put back so that
-// it can be reread by parseExp() or an alternative version of
-// parseExp() must be called.
-//
-// If EOF is reached (i.e., if the scanner returns a NULL) token,
-// the parser returns a NULL tree.  In case of a parse error, the
-// parser discards the offending token (which probably was a DOT
-// or an RPAREN) and attempts to continue parsing with the next token.
+// Parser -- Recursive-descent parser for the Scheme printer and interpreter
 
 package Parse;
 
-import Tree.Node;
+import Tree.*;
+
+import Tokens.*;
 
 public class Parser {
     private Scanner scanner;
+    private Token lookahead = null;  // one-token lookahead
 
     public Parser(Scanner s) {
         scanner = s;
     }
 
+    // Utility: get next token, using lookahead if available
+    private Token nextToken() {
+        if (lookahead != null) {
+            Token t = lookahead;
+            lookahead = null;
+            return t;
+        } else {
+            return scanner.getNextToken();
+        }
+    }
+
+    // Utility: push back one token
+    private void pushBack(Token t) {
+        lookahead = t;
+    }
+
+    // Parse an expression (exp)
     public Node parseExp() {
-        // TODO: write code for parsing an exp
-        return null;
+        Token tok = nextToken();
+        if (tok == null) return null;  // EOF
+
+        switch (tok.getType()) {
+            case TRUE:
+                return BoolLit.TRUE;
+            case FALSE:
+                return BoolLit.FALSE;
+            case INT:
+                return new IntLit(tok.getIntVal());
+            case STRING:
+                return new StringLit(tok.getStrVal());
+            case IDENT:
+                return new Ident(tok.getName().toLowerCase()); // Scheme is case-insensitive
+            case QUOTE:
+                // 'exp => (quote exp)
+                Node quoted = parseExp();
+                return new Cons(new Ident("quote"), new Cons(quoted, Nil.NIL));
+            case LPAREN:
+                return parseRest();
+            case RPAREN:
+            case DOT:
+            default:
+                // unexpected token
+                System.err.println("Unexpected token: " + tok.getType());
+                return parseExp(); // skip and try next
+        }
     }
 
+    // Parse a list (rest)
     protected Node parseRest() {
-        // TODO: write code for parsing rest
-        return null;
-    }
+        Token tok = nextToken();
 
-    // TODO: Add any additional methods you might need.
+        // Empty list
+        if (tok.getType() == TokenType.RPAREN) {
+            return Nil.NIL;
+        }
+
+        // Put back first token of the first element
+        pushBack(tok);
+        Node first = parseExp();
+
+        // Check if next token is a dot (for dotted pair)
+        tok = nextToken();
+        if (tok.getType() == TokenType.DOT) {
+            Node second = parseExp();
+            Token closing = nextToken();
+            if (closing.getType() != TokenType.RPAREN) {
+                System.err.println("Expected ')' after dotted pair");
+            }
+            return new Cons(first, second);
+        } else if (tok.getType() == TokenType.RPAREN) {
+            return new Cons(first, Nil.NIL);
+        } else {
+            // Regular list: put back token and parse rest recursively
+            pushBack(tok);
+            Node rest = parseRest();
+            return new Cons(first, rest);
+        }
+    }
 }
