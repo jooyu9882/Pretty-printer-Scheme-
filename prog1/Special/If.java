b7a8fced03780a818593c6d2a1ed1023132f72ae
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 18:36:07 -0600

    Update If.java

--- prog1/Special/If.java
+++ prog1/Special/If.java
@@ -1,11 +1,23 @@
-// If -- Parse tree node strategy for printing the special form if
-
 package Special;
 
-import Tree.Node;
+import Tree.*;
 
 public class If extends Special {
- 
     public void print(Node t, int n, boolean p) {
+        Cons c = (Cons) t;
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.print("(if ");
+        c.getCdr().getCar().print(0);
+        Node rest = c.getCdr().getCdr();
+        if (!rest.isNull()) {
+            System.out.println();
+            rest.getCar().print(n + 2);
+            Node last = rest.getCdr();
+            if (!last.isNull()) {
+                last.getCar().print(n + 2);
+            }
+        }
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.println(")");
     }
 }
