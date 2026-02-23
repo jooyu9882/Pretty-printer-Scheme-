22e959c9d7bee538cd19e8acfff172a08efe59b1
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 18:37:48 -0600

    Update Cond.java

--- prog1/Special/Cond.java
+++ prog1/Special/Cond.java
@@ -1,11 +1,18 @@
-// Cond -- Parse tree node strategy for printing the special form cond
-
 package Special;
 
-import Tree.Node;
+import Tree.*;
 
 public class Cond extends Special {
- 
     public void print(Node t, int n, boolean p) {
+        Cons c = (Cons) t;
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.println("(cond");
+        Node rest = c.getCdr();
+        while (!rest.isNull()) {
+            rest.getCar().print(n + 2);
+            rest = rest.getCdr();
+        }
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.println(")");
     }
 }
