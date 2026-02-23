dfc49daf82e49a536d9d26c2579b02e7c14765c0
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 18:37:23 -0600

    Update Let.java

--- prog1/Special/Let.java
+++ prog1/Special/Let.java
@@ -1,11 +1,18 @@
-// Let -- Parse tree node strategy for printing the special form let
-
 package Special;
 
-import Tree.Node;
+import Tree.*;
 
 public class Let extends Special {
- 
     public void print(Node t, int n, boolean p) {
+        Cons c = (Cons) t;
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.println("(let");
+        Node rest = c.getCdr();
+        while (!rest.isNull()) {
+            rest.getCar().print(n + 2);
+            rest = rest.getCdr();
+        }
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.println(")");
     }
 }
