ba053e1c894db27d148ffb850079986fa516af79
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 18:35:48 -0600

    Update Begin.java

--- prog1/Special/Begin.java
+++ prog1/Special/Begin.java
@@ -1,11 +1,18 @@
-// Begin -- Parse tree node strategy for printing the special form begin
-
 package Special;
 
-import Tree.Node;
+import Tree.*;
 
 public class Begin extends Special {
- 
     public void print(Node t, int n, boolean p) {
+        Cons c = (Cons) t;
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.println("(begin");
+        Node rest = c.getCdr();
+        while (!rest.isNull()) {
+            rest.getCar().print(n + 2);
+            rest = rest.getCdr();
+        }
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.println(")");
     }
 }
