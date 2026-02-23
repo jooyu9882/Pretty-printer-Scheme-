d8b5bab5e9dcd881ad5621895b2093cc7601d93f
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 18:35:17 -0600

    Update Regular.java

--- prog1/Special/Regular.java
+++ prog1/Special/Regular.java
@@ -1,11 +1,29 @@
-// Regular -- Parse tree node stratagy for printing regular lists
-
 package Special;
 
-import Tree.Node;
+import Tree.*;
 
 public class Regular extends Special {
- 
     public void print(Node t, int n, boolean p) {
+        Cons c = (Cons) t;
+        if (!p) {
+            for (int i = 0; i < n; i++)
+                System.out.print(" ");
+            System.out.print("(");
+        }
+
+        c.getCar().print(0);
+
+        Node next = c.getCdr();
+        if (!next.isNull()) {
+            if (next.isPair()) {
+                System.out.print(" ");
+                next.print(0, true);
+            } else {
+                System.out.print(" . ");
+                next.print(0);
+            }
+        } else {
+            System.out.println(")");
+        }
     }
 }
