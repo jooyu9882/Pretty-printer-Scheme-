49e4327d1466e631f53598379619eb35a9ceee6a
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 18:36:44 -0600

    Update Lambda.java

--- prog1/Special/Lambda.java
+++ prog1/Special/Lambda.java
@@ -1,11 +1,20 @@
-// Lambda -- Parse tree node strategy for printing the special form lambda
-
 package Special;
 
-import Tree.Node;
+import Tree.*;
 
 public class Lambda extends Special {
- 
     public void print(Node t, int n, boolean p) {
+        Cons c = (Cons) t;
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.print("(lambda ");
+        c.getCdr().getCar().print(0);
+        Node rest = c.getCdr().getCdr();
+        while (!rest.isNull()) {
+            System.out.println();
+            rest.getCar().print(n + 2);
+            rest = rest.getCdr();
+        }
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.println(")");
     }
 }
