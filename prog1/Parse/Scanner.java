e6ec50d6cf3183b0e286d04af198aac4b528adaf
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 21:10:47 -0600

    Update Scanner.java

--- prog1/Parse/Scanner.java
+++ prog1/Parse/Scanner.java
@@ -1,5 +1,3 @@
-// Scanner -- The lexical analyzer for the Scheme printer and interpreter.
-
 package Parse;
 
 import java.io.IOException;
@@ -13,145 +11,140 @@ import Tokens.IntToken;
 import Tokens.StrToken;
 
 public class Scanner {
-	private PushbackInputStream in;
 
-	// Maximum length of strings and identifers
-	private int BUFSIZE = 1000;
-	private byte[] buf = new byte[BUFSIZE];
+```
+private PushbackInputStream in;
 
-	public Scanner(InputStream i) {
-		in = new PushbackInputStream(i);
-	}
+private int BUFSIZE = 1000;
+private byte[] buf = new byte[BUFSIZE];
 
-	public Token getNextToken() {
-		int ch;
+public Scanner(InputStream i) {
+    in = new PushbackInputStream(i);
+}
 
-		try {
-			// It would be more efficient if we'd maintain our own
-			// input buffer and read characters out of that
-			// buffer, but reading individual characters from the
-			// input stream is easier.
-			ch = in.read();
+public Token getNextToken() {
 
-			// TODO: Skip white space and comments
-			while (ch == ' ' || ch == '\t' || ch == '\n' || ch == ';') {
+    int ch;
 
-				if (ch == ';') {
-					while (ch != '\n' && ch != -1)
-						ch = in.read();
-				}
+    try {
 
-				else
-					ch = in.read();
-			}
-			// Return null on EOF
-			if (ch == -1)
-				return null;
+        /* ----------- Skip whitespace and comments ----------- */
+        while (true) {
+            ch = in.read();
 
-			// Special characters
-			else if (ch == '\'')
-				return new Token(TokenType.QUOTE);
-			else if (ch == '(')
-				return new Token(TokenType.LPAREN);
-			else if (ch == ')')
-				return new Token(TokenType.RPAREN);
-			else if (ch == '.')
-				// We ignore the special identifier `...'.
-				return new Token(TokenType.DOT);
+            if (ch == -1)
+                return null;
 
-			// Boolean constants
-			else if (ch == '#') {
-				ch = in.read();
+            if (Character.isWhitespace(ch))
+                continue;
 
-				if (ch == 't')
-					return new Token(TokenType.TRUE);
-				else if (ch == 'f')
-					return new Token(TokenType.FALSE);
-				else if (ch == -1) {
-					System.err.println("Unexpected EOF following #");
-					return null;
-				} else {
-					System.err.println("Illegal character '" +
-							(char)ch + "' following #");
-					return getNextToken();
-				}
-			}
+            if (ch == ';') {
+                while (ch != '\n' && ch != -1)
+                    ch = in.read();
+                continue;
+            }
 
-			// String constants
-			else if (ch == '"') {
-				// TODO: Scan a string into the buffer variable buf
-				int i = 0;
-				while (true) {
-					ch = in.read();
-					if (ch == '"') {
-						return new StrToken(new String(buf,0, i));
-					} 
+            break;
+        }
 
-					else if (ch == -1) {
-						System.err.println("Unexpected EOF in string");
-						return null;
-					}
+        /* ----------- Single char tokens ----------- */
+        if (ch == '\'')
+            return new Token(TokenType.QUOTE);
 
-					else {
-						buf[i++] = (byte)ch;
-					}
-				}
-			}
+        if (ch == '(')
+            return new Token(TokenType.LPAREN);
 
-			// Integer constants
-			else if (ch >= '0' && ch <= '9') {
-				int i = ch - '0';
-				// TODO: scan the number and convert it to an integer
-				 while (true) {
-					ch = in.read();
+        if (ch == ')')
+            return new Token(TokenType.RPAREN);
 
-					if (ch >= '0' && ch <= '9') {
-						i = i * 10 + (ch - '0');
-					}
-					
-					else {
-						in.unread(ch);
-						break;
-					}
-				}
+        if (ch == '.')
+            return new Token(TokenType.DOT);
 
-				// Put the character after the integer back into the input
-				// in.unread(ch);
-				return new IntToken(i);
-			}
+        /* ----------- Boolean ----------- */
+        if (ch == '#') {
+            ch = in.read();
 
-			// Identifiers
-			else if (ch >= 'A' && ch <= 'Z'
-				/* or ch is some other valid first character for an identifier */) {
-				// TODO: scan an identifier into the buffer variable buf
-				int i = 0;
-				while (true) {
-					buf[i++] = (byte)ch;
-					ch = in.read();
+            if (ch == 't')
+                return new Token(TokenType.TRUE);
+            if (ch == 'f')
+                return new Token(TokenType.FALSE);
 
-					if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
-						continue;
-					}
-					
-					else {
-						in.unread(ch);
-						break;
-					}
-				}
-				// Put the character after the identifier back into the input
-				// in.unread(ch);
+            System.err.println("Illegal boolean");
+            return null;
+        }
 
-				return new IdentToken(new String(buf, 0, i));
-			}
+        /* ----------- String ----------- */
+        if (ch == '"') {
 
-			// Illegal character
-			else {
-				System.err.println("Illegal input character '" + (char)ch + '\'');
-				return getNextToken();
-			}
-		} catch (IOException e) {
-			System.err.println("IOException: " + e.getMessage());
-			return null;
-		}
-	}
+            int i = 0;
+
+            while (true) {
+                ch = in.read();
+
+                if (ch == '"')
+                    return new StrToken(new String(buf, 0, i));
+
+                if (ch == -1) {
+                    System.err.println("EOF inside string");
+                    return null;
+                }
+
+                buf[i++] = (byte) ch;
+            }
+        }
+
+        /* ----------- Integer ----------- */
+        if (Character.isDigit(ch)) {
+
+            int value = ch - '0';
+
+            while (true) {
+                ch = in.read();
+
+                if (!Character.isDigit(ch))
+                    break;
+
+                value = value * 10 + (ch - '0');
+            }
+
+            if (ch != -1)
+                in.unread(ch);
+
+            return new IntToken(value);
+        }
+
+        /* ----------- Identifier ----------- */
+        if (Character.isLetter(ch) || "+-*/<>=!?".indexOf(ch) != -1) {
+
+            int i = 0;
+            buf[i++] = (byte) Character.toLowerCase(ch);
+
+            while (true) {
+                ch = in.read();
+
+                if (Character.isLetterOrDigit(ch) ||
+                    "+-*/<>=!?".indexOf(ch) != -1) {
+                    buf[i++] = (byte) Character.toLowerCase(ch);
+                }
+                else {
+                    if (ch != -1)
+                        in.unread(ch);
+                    break;
+                }
+            }
+
+            return new IdentToken(new String(buf, 0, i));
+        }
+
+        /* ----------- Illegal ----------- */
+        System.err.println("Illegal character: " + (char) ch);
+        return getNextToken();
+
+    } catch (IOException e) {
+        System.err.println("IO Error");
+        return null;
+    }
+}
+```
+
 }
