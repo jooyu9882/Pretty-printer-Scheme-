373504971de790cad736b18cfb23e021e6a47291
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 18:38:09 -0600

    Update Set.java

--- prog1/Special/Set.java
+++ prog1/Special/Set.java
@@ -1,11 +1,17 @@
-// Set -- Parse tree node strategy for printing the special form set!
-
 package Special;
 
-import Tree.Node;
+import Tree.*;
 
 public class Set extends Special {
- 
     public void print(Node t, int n, boolean p) {
+        Cons c = (Cons) t;
+        for (int i = 0; i < n; i++) System.out.print(" ");
+        System.out.print("(set! ");
+        c.getCdr().getCar().print(0);
+        Node rest = c.getCdr().getCdr();
+        if (!rest.isNull()) {
+            rest.getCar().print(0);
+        }
+        System.out.println(")");
     }
 }
