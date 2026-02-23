9848abd233d4ba615adadd96f6a77be13209cad7
TroyBoatner <143111964+TroyBoatner@users.noreply.github.com>
2026-02-22 18:39:56 -0600

    Update Cons.java

--- prog1/Tree/Cons.java
+++ prog1/Tree/Cons.java
@@ -1,17 +1,6 @@
-// Cons -- Parse tree node class for representing a Cons node
-
 package Tree;
 
-import Special.Begin;
-import Special.Cond;
-import Special.Define;
-import Special.If;
-import Special.Lambda;
-import Special.Let;
-import Special.Quote;
-import Special.Regular;
-import Special.Set;
-import Special.Special;
+import Special.*;
 
 public class Cons extends Node {
     private Node car;
@@ -24,46 +13,26 @@ public class Cons extends Node {
         parseList();
     }
 
-    // parseList() `parses' special forms, constructs an appropriate
-    // object of a subclass of Special, and stores a pointer to that
-    // object in variable form. It would be possible to fully parse
-    // special forms at this point. Since this causes complications
-    // when using (incorrect) programs as data, it is easiest to let
-    // parseList only look at the car for selecting the appropriate
-    // object from the Special hierarchy and to leave the rest of
-    // parsing up to the interpreter.
-    void parseList() {
-    
+    // Determines the form for printing (special forms or regular list)
+    private void parseList() {
+        form = new Regular();
 
-    // TODO: Add any helper functions for parseList
-
-    form = new Regular();
-
-    if (car instanceof Ident) {
-
-        String name = ((Ident) car).getName();
-
-        if (name.equals("if"))
-            form = new If();
-        else if (name.equals("lambda"))
-            form = new Lambda();
-        else if (name.equals("quote"))
-            form = new Quote();
-        else if (name.equals("begin"))
-            form = new Begin();
-        else if (name.equals("define"))
-            form = new Define();
-        else if (name.equals("set!"))
-            form = new Set();
-        else if (name.equals("let"))
-            form = new Let();
-        else if (name.equals("cond"))
-            form = new Cond();
-    }
+        if (car instanceof Ident) {
+            String name = ((Ident) car).getName();
+            switch (name) {
+                case "if":      form = new If(); break;
+                case "lambda":  form = new Lambda(); break;
+                case "quote":   form = new Quote(); break;
+                case "begin":   form = new Begin(); break;
+                case "define":  form = new Define(); break;
+                case "set!":    form = new Set(); break;
+                case "let":     form = new Let(); break;
+                case "cond":    form = new Cond(); break;
+            }
+        }
     }
 
-    // to the class hierarchy as needed.
-
+    // Printing
     public void print(int n) {
         form.print(this, n, false);
     }
@@ -71,4 +40,18 @@ public class Cons extends Node {
     public void print(int n, boolean p) {
         form.print(this, n, p);
     }
+
+    // Getters and setters
+    public Node getCar() { return car; }
+    public Node getCdr() { return cdr; }
+
+    public void setCar(Node a) { 
+        car = a; 
+        parseList(); // Re-evaluate special form when car changes
+    }
+
+    public void setCdr(Node d) { cdr = d; }
+
+    // Type check
+    public boolean isPair() { return true; }
 }
